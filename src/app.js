7d21af9812bebf5788905bf50ce16cea1730b837
Elibergh Santiago <89936442+undefined@users.noreply.github.com>
2022-02-14 19:39:49 +0000

    Push from first

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import {LightningElement} from "lwc";
 
 export default class App extends LightningElement {
-  dados = {nome:"Elibergh", idade:26};
+  data = [
+    {cod:"P-001", nome: "Produto A", preco:"200", estoque:"sim"},
+    {cod:"P-002", nome: "Produto B", preco:"300", estoque:"sim"},
+    {cod:"P-003", nome: "Produto C", preco:"400", estoque:"sim"},
+    {cod:"P-004", nome: "Produto D", preco:"500", estoque:"sim"},
+    {cod:"P-005", nome: "Produto E", preco:"600", estoque:"sim"},
+    {cod:"P-006", nome: "Produto F", preco:"700", estoque:"sim"}
+  ];
 }
